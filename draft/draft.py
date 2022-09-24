4efb95868062fba06d214d8bab1f3eb7a07e2ba5
SlavaShaiko <v.shaiko2011@yandex.ru>
2022-09-24 11:12:10 +0300

    added new file

--- draft/draft.py
+++ draft/draft.py
@@ -1,9 +1,19 @@
 
-
-
-for i in range(5) :
+""" max = 0
+for i in range(5):
     x = int(input())
-    print(x)
-     
-    
-    
+    if max < x: max = x
+print(max) """
+
+
+""" n = int(input())
+for i in range(-n, n + 1):
+    print(i, end=' ') """
+
+
+n = float(input())
+n = (int(n*10)) % 10
+if n == 0:
+    print('нет')
+else:
+    print(n)
