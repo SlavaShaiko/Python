70442b36ff948b2021e6c708ce634968c221f358
SlavaShaiko <v.shaiko2011@yandex.ru>
2022-09-24 11:45:16 +0300

    added new file in folder draft

--- draft/draft.py
+++ draft/draft.py
@@ -11,9 +11,15 @@ for i in range(-n, n + 1):
     print(i, end=' ') """
 
 
-n = float(input())
+""" n = float(input())
 n = (int(n*10)) % 10
 if n == 0:
     print('нет')
 else:
-    print(n)
+    print(n) """
+
+n = int(input())
+if (n % 10 == 0 or n % 15 == 0) and n % 30 != 0:
+    print('кратно')
+else:
+    print('нет')
